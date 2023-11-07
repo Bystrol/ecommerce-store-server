e5ebf9687dfb04ef25f26e945fcb0f125b65884a
Michal <michalbystryk14@wp.pl>
2023-11-07 16:01:48 +0100

    Add user model

---
+++ src/models/user.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from "mongoose"
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  cart: {
+    type: Array,
+    ref: "Product",
+  },
+})
+
+export default model("User", userSchema)
