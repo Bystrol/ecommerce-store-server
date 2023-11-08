beffbbf97ea19cfdb78ba228de6d7876ab95d051
Michal <michalbystryk14@wp.pl>
2023-11-08 17:57:41 +0100

    Remove auth token and change response message

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -2,10 +2,9 @@ import { Request, Response } from "express"
 import { validationResult } from "express-validator"
 import User from "../models/user"
 import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
 
 export const registerUser = async (req: Request, res: Response) => {
-  const { username, email, password, confirmPassword } = req.body
+  const { username, email, password } = req.body
   const errors = validationResult(req)
 
   if (!errors.isEmpty()) {
@@ -34,14 +33,5 @@ export const registerUser = async (req: Request, res: Response) => {
 
   await user.save()
 
-  const authToken = jwt.sign(
-    {
-      username,
-      email,
-    },
-    "ecommerce_store_register_secret",
-    { expiresIn: "1h" }
-  )
-
-  return res.status(200).json({ message: "User created", authToken })
+  return res.status(200).json({ message: "User registered" })
 }
