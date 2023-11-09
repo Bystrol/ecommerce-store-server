1377c3b12831698e94a85565e0de2a9a87d4b570
Michal <michalbystryk14@wp.pl>
2023-11-09 20:16:26 +0100

    Add login controller

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express"
 import { validationResult } from "express-validator"
 import User from "../models/user"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 export const registerUser = async (req: Request, res: Response) => {
   const { username, email, password } = req.body
@@ -13,25 +14,68 @@ export const registerUser = async (req: Request, res: Response) => {
       .json({ message: "Invalid credentials", errors: errors.array() })
   }
 
-  const userExists = await User.findOne({ email })
+  try {
+    const userExists = await User.findOne({ email })
 
-  if (userExists) {
-    return res
-      .status(409)
-      .json({ message: "User with this email already exists" })
+    if (userExists) {
+      return res
+        .status(409)
+        .json({ message: "User with this email already exists" })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12)
+
+    const user = new User({
+      username,
+      email,
+      password: hashedPassword,
+      role: "user",
+      cart: [],
+    })
+
+    await user.save()
+
+    return res.status(201).json({ message: "User registered" })
+  } catch (error) {
+    console.log(error)
+    throw new Error("Something went wrong")
   }
+}
 
-  const hashedPassword = await bcrypt.hash(password, 12)
+export const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body
 
-  const user = new User({
-    username,
-    email,
-    password: hashedPassword,
-    role: "user",
-    cart: [],
-  })
+  try {
+    const user = await User.findOne({ email })
 
-  await user.save()
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "User with this email doesn't exist" })
+    }
 
-  return res.status(200).json({ message: "User registered" })
+    const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordCorrect) {
+      return res.status(422).json({ message: "Incorrect password" })
+    }
+
+    const authToken = jwt.sign(
+      {
+        email,
+        password,
+      },
+      "JWT_SECRET",
+      { expiresIn: "1h" }
+    )
+
+    return res.status(200).json({
+      message: "User successfully logged in",
+      authToken,
+      userRole: user.role,
+    })
+  } catch (error) {
+    console.log(error)
+    throw new Error("Something went wrong")
+  }
 }
