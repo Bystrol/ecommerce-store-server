2ad353ce1477ca7aa81e98ff9483ac72cdb1beae
Michal <michalbystryk14@wp.pl>
2023-11-10 22:42:52 +0100

    Add product model

---
+++ src/models/product.ts
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose"
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  isAvailable: {
+    type: Boolean,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+})
+
+export default model("Product", productSchema)
